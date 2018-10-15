cc27fa32ff919aa7f2106fdf9d67d357fe8b7395
EPDollinger <44129015+EPDollinger@users.noreply.github.com>
2018-10-15 14:51:51 -0700

    Fixed the equation and the plot

--- HW1part3.m
+++ HW1part3.m
@@ -1,5 +1,14 @@
 function[ySol] = HW1part3
     syms h(r)
-    ode = diff(h,r,2)/r+diff(h,r)/r^2- diff(h,r,4) == 0;
-    ySol(x) = dsolve(ode)
+    syms r
+    Dh = diff(h,r);
+    ode = -diff(h,r,2)/r+diff(h,r)/r^2 +2*diff(h,r,3)+ r*diff(h,r,4) == 0;
+    cond1 = Dh(5) == 0;
+    cond2 = Dh(100) == 0;
+    cond3 = h(5) == 50;
+    cond4 = h(100) == 0;
+    conds = [cond1 cond2 cond3 cond4];
+    ySol(r) = dsolve(ode,conds);
+    x = 5:1:100;
+    plot(x,ySol(x))
 end
